49cb74415b782b0df1f1b673e97bf0e1eff32540
AleshaMolodec <aleks.fandoren@gmail.com>
2020-04-03 14:57:36 +0300

    Made textbox template

---
+++ CookingApp/ViewModel/LoginVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace CookingApp.ViewModel
+{
+    class LoginVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName)); // если событие PropertyChanged не null, то вызываем событие и передаём имя свойства
+        }
+
+        private string _login;
+        public string login
+        {
+            get { return _login; }
+            set
+            {
+                _login = value;
+            }
+        }
+    }
+}
